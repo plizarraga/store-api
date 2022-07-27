8645ddba091cb6033c876911b8ac5aa1aa5ecf10
Pedro Lizarraga <plizarraga86@gmail.com>
2022-07-27 08:40:09 -0700

    added CORS and clean products controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ProductsController < Api::V1::BaseController
-  before_action :set_product, only: %i[ show update destroy ]
+  before_action :set_product, only: %i[ show ]
   
   def index
     pageSize = 5
@@ -27,28 +27,6 @@ class Api::V1::ProductsController < Api::V1::BaseController
     render json: ProductBlueprint.render(@product)
   end
 
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      render json: @product, status: :created, location: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @product.update(product_params)
-      render json: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @product.destroy
-  end
-
   private
     def set_product
       @product = Product.includes(:product_type, :product_brand).find(params[:id])
@@ -56,10 +34,6 @@ class Api::V1::ProductsController < Api::V1::BaseController
 
     def product_params
       params.require(:product).permit(:name, :description, :price, :product_type_id, :product_brand_id)
-    end
-
-    def set_order_options
-      Product::ORDER_BY.fetch(params[:sort]&.to_sym, Product::ORDER_BY[:nameAsc])
     end
 
     def set_categories_filter
