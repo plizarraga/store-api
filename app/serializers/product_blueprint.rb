920fc0822e2746de0b830d82ad176394e19ac258
Pedro Lizarraga <plizarraga86@gmail.com>
2022-07-27 22:30:54 -0700

    added picture_url to product serialize

--- app/serializers/product_blueprint.rb
+++ app/serializers/product_blueprint.rb
@@ -1,7 +1,7 @@
 class ProductBlueprint < Blueprinter::Base
     identifier :id
       
-    fields :name, :description, :price
+    fields :name, :description, :price, :picture_url
     association :product_type, blueprint: ProductTypeBlueprint
     association :product_brand, blueprint: ProductBrandBlueprint
   end
