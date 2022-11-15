0704ea614ae0206faf4ddedf9372d7771a3fced3
Pedro Lizarraga <plizarraga86@gmail.com>
2022-11-14 18:42:36 -0800

    Convert product price to float (serializer)

--- app/serializers/product_blueprint.rb
+++ app/serializers/product_blueprint.rb
@@ -1,7 +1,11 @@
 class ProductBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :name, :description, :price, :picture_url
+  fields :name, :description, :picture_url
+  
+  field :price do |product, options|
+    product.price.to_f
+  end
 
   association :product_type, blueprint: ProductTypeBlueprint
   association :product_brand, blueprint: ProductBrandBlueprint
