787b5674236654e8b74194cd8bc1b4a9a8f21378
Pedro Lizarraga <plizarraga86@gmail.com>
2022-12-08 19:49:22 -0800

    Deep transform keys for serializers

--- app/serializers/transformers/lower_camel_transformer.rb
+++ app/serializers/transformers/lower_camel_transformer.rb
@@ -1,5 +1,5 @@
 class LowerCamelTransformer < Blueprinter::Transformer
     def transform(hash, _object, _options)
-      hash.transform_keys! { |key| key.to_s.camelize(:lower).to_sym }
+      hash.deep_transform_keys! { |key| key.to_s.camelize(:lower).to_sym }
     end
 end
