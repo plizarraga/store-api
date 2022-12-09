d2203c5123ecd4401e0be3a2fc17a5c99c075be9
Pedro Lizarraga <plizarraga86@gmail.com>
2022-12-08 20:09:14 -0800

    Renamed fields for order item serializer

--- app/serializers/order_item_blueprint.rb
+++ app/serializers/order_item_blueprint.rb
@@ -1,8 +1,8 @@
 class OrderItemBlueprint < Blueprinter::Base
-  identifier :id
-
-  fields :product_item_id, :product_item_name,
-    :product_item_picture_url, :quantity
+  field :product_item_id, name: :product_id
+  field :product_item_name, name: :product_name
+  field :product_item_picture_url, name: :picture_url
+  field :quantity
 
   field :price do |order_item, options|
     order_item.price.to_f
